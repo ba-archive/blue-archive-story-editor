6deeb4ed1f81e14b5415e605206fa0eb4a5c0d39
diyigemt 111 <qwe1355247243@gmail.com>
2023-04-21 11:59:55 +0800

    chore: 加一些约束

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
   },
   plugins: ["vue", "@typescript-eslint", "import"],
   rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
     "no-console": "off",
     "no-plusplus": "off",
     "no-param-reassign": "off",
@@ -40,5 +41,25 @@ module.exports = {
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": ["error"],
     "no-underscore-dangle": "off", // 允许使用带有下划线的成员变量, this._foo 表明这是一个私有变量
+    semi: "error",
+    "linebreak-style": ["error", "unix"],
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "ignore",
+      },
+    ],
+    "generator-star-spacing": "off",
+    quotes: ["error", "double", { avoidEscape: true }],
+    "space-before-function-paren": ["error", "never"],
+    "no-unused-expressions": "off",
+    "object-curly-spacing": ["error", "always"],
+    "@typescript-eslint/no-unused-vars": "off",
+    "arrow-parens": ["error", "always"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
   },
 };
